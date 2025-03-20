78313c88dd447f0002517470f9af81c88ff88b28
N-akashiya <571522556@qq.com>
2025-03-20 09:33:37 +0800

    +id

--- src/main/java/com/dbs/tpc_benchmark/typings/entity/User.java
+++ src/main/java/com/dbs/tpc_benchmark/typings/entity/User.java
@@ -6,6 +6,9 @@ import jakarta.persistence.*;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(unique = true, nullable = false)
     private String name;
 
@@ -26,6 +29,14 @@ public class User {
         this.role = role;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     public String getName() {
         return name;
     }
