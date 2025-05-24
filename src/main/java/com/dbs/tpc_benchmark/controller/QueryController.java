0918b9cbc3117728311ebf16149948e8b9e15994
Easter1995 <1073208733@qq.com>
2025-05-24 13:55:51 +0800

    feat: 业务查询接口

---
+++ src/main/java/com/dbs/tpc_benchmark/controller/QueryController.java
@@ -0,0 +1,9 @@
+package com.dbs.tpc_benchmark.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/query")
+public class QueryController {
+}
---
+++ src/main/java/com/dbs/tpc_benchmark/typings/vo/ClientInfoVO.java
@@ -0,0 +1,12 @@
+package com.dbs.tpc_benchmark.typings.vo;
+
+public class ClientInfoVO {
+    private int cuskey;
+    private String name;
+    private String address;
+    private String nation;
+    private String phone;
+    private float acctbal;
+    private String mktsegment;
+    private String comment;
+}
